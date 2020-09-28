0e3906eaec4e7284a189be994c166a09c9cb1cb7
limdes <lim_des@yoondisk.co.kr>
2020-09-28 19:52:18 +0900

    chore: util.js, video.js

--- html5_player/php_player/js/util.js
+++ html5_player/php_player/js/util.js
@@ -1,6 +1,11 @@
+//replaceAll prototype 선언
+String.prototype.replaceAll_ = function(org, dest) {
+    return this.split(org).join(dest);
+}
+
 function numFormat(variable) { variable = Number(variable).toString(); if(Number(variable) < 10 && variable.length == 1) variable = "0" + variable; return variable; }
 	
-	function rpad(str, padLen, padStr) {
+function rpad(str, padLen, padStr) {
 		if (padStr.length > padLen) {
 			return str + "";
 		}
@@ -20,4 +25,61 @@ function log_(a){
 		let milliseconds = today.getMilliseconds(); // 밀리초
 		let stamp=numFormat(hours) + ':' + numFormat(minutes) + ':' + numFormat(seconds) + '.' + milliseconds;
 		$('#info').val($('#info').val()+"[VideoJs Event] "+rpad(a,10," ")+" ("+stamp+")\r\n");	
+}
+
+var TimeFormat_str = function(totalsecond) {
+		var second = Math.floor(totalsecond % 60);
+		var minute = Math.floor(Math.floor(totalsecond / 60) % 60) ;
+		var hour = Math.floor(totalsecond / 3600);
+		if (second<0){second=0;}
+		if (minute<0){minute=0;}
+		if (hour<0){hour=0;}
+
+		return hour+":"+((minute < 10)?"0":"")+minute+":" + ((second < 10)?"0":"")+second;
+}
+
+var getCookie_str = function(check_value) {
+		check_value = check_value + "=";
+		var x = 0;
+		while (x <= document.cookie.length) {
+			var y = (x + check_value.length);
+			if (document.cookie.substring(x, y) == check_value) {
+				if ((endOfCookie = document.cookie.indexOf(";", y)) == -1) {
+					endOfCookie = document.cookie.length;
+				}
+				return unescape(document.cookie.substring(y, endOfCookie));
+			}
+			x = document.cookie.indexOf(" ", x) + 1;
+			if (x == 0) {
+				break;
+			}
+		}
+		return "";
+}
+
+var setCookie_str = function(check_value, set_value, day_value) {
+		var todayDate = new Date();
+		todayDate.setDate(todayDate.getDate() + day_value);
+		document.cookie = check_value + "=" + escape(set_value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+}
+
+var setMediaBookMarkStyle = function(objMediaBox, objMediaPlayer, strBookmarkName, objThis, intThisIndex, intBookmarkValue) {
+		$.data(objThis,"BookmarkValue",intBookmarkValue);
+		$(objThis).html(TimeFormat_str(intBookmarkValue));
+		objMediaBox.find(".aBookMarkReSet_" + intThisIndex).remove();
+		$(objThis).parent().append(" <a href=\"javascript:void(0)\" class=\"aBookMarkReSet_" + intThisIndex + "\">[수정]</a>");
+		objMediaBox.find(".aBookMarkReSet_" + intThisIndex).click(function() {
+			intBookmarkValue = player.currentTime();  
+			setCookie_str(strBookmarkName + intThisIndex, intBookmarkValue, 365);
+			setMediaBookMarkStyle(objMediaBox, objMediaPlayer, strBookmarkName, objThis, intThisIndex, intBookmarkValue);
+		});
+		$(objThis).unbind("click");
+		$(objThis).click(function() {
+			exeMediaMoveBookMark(objMediaBox, objMediaPlayer, objThis);
+		});
+}
+
+var exeMediaMoveBookMark = function(objMediaBox, objMediaPlayer, objThis) {
+		var intBookmarkValue = parseInt($.data(objThis,"BookmarkValue"));
+		player.currentTime(intBookmarkValue);
 }
